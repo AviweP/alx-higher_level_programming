843fef82635109776ded39e01f125893a63ee70f
AviweP <prudencedonovanmalopi@gmail.com>
2023-11-30 13:23:32 -0800

    my argiment counter

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    argv = len(sys.argv) - 1
+    if argv == 0:
+        print("0 argument.")
+    elif argv  == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argv))
+        for i in range(argv):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
