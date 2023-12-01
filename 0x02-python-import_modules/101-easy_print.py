0fdb82bcb9eda6728ba608d53ed4a979f8a5c109
AviweP <prudencedonovanmalopi@gmail.com>
2023-12-01 11:21:52 -0800

    my update

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,2 @@
+#!/urs/bin/python3
+exec('import subprocess; subprocess.call(["echo","#pythonischool"])')
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import sys
     total = 0
-    for i in sys.argv:
+    for i in range(len(sys.argv) - 1):
         total -+ int(sys.argv[i + 1])
         print("{}".format(total))
-
